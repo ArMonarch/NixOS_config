f22ab82f4ed4e5fc24ecc120718dde1616a4b52e
ArMonarch <praffulthapa12@gmail.com>
2025-06-27 10:57:42 +0545

    feat: removed nix-hardware package

--- flake.nix
+++ flake.nix
@@ -4,10 +4,12 @@
   inputs = {
     # NixOS official package source, using the nixos-25.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
 
     nixosConfigurations.frenzfries = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -19,7 +21,6 @@
     };
 
     nixosConfigurations.legion = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
