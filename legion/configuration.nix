b791540e70a787d4ae336f9408ab82b3f7556fbf
ArMonarch <praffulthapa12@gmail.com>
2025-06-27 10:58:03 +0545

    feat: added nix-grub-theme submodule

--- legion/configuration.nix
+++ legion/configuration.nix
@@ -2,8 +2,11 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
+let
+  theme-name = "kakaluo"; # [ "changli" "jianxin" "jinxi" "kakaluo" "yinlin" ]
+in
 {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -15,12 +18,17 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
+  boot.loader.timeout = 10;
   boot.loader.systemd-boot.enable = false;
   boot.loader.grub.enable = true;
   boot.loader.grub.useOSProber = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.devices = [ "nodev" ];
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # Grub Theme Install
+  # boot.loader.grub.extraFiles = extraFiles;
+  boot.loader.grub.theme = "/home/frenzfries/Dev/NixOS-Dotfile/NixOS-grub-theme/theme/${theme-name}";
 
   networking.hostName = "frenzfries-NixOS"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -162,11 +170,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
