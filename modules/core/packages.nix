4c87c5512db6cec68a022268f01c53af24f5d405
ArMonarch <praffulthapa12@gmail.com>
2025-07-19 13:25:06 +0545

    feat: great utils added

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -6,16 +6,28 @@
 
   nixpkgs.config.allowUnfree = true;
   environment.defaultPackages = with pkgs; [
+    brightnessctl # For Screen Brightness Control
     cmatrix # Matrix Movie Effect In Terminal
     cowsay # Great Fun Terminal Program
+    eza # Beautiful ls Replacement
+    ffmpeg # Terminal Video / Audio Editing
+    gedit # Simple Graphical Text Editor
     ghostty # Great Terminal Emulator
     git # version manager
+    htop # Simple Terminal Based System Monitor
+    killall # For Killing All Instances Of Programs
     lazygit # Terminal UI for git
+    libnotify # For Notifications
+    lshw # Detailed Hardware Information
+    ncdu # Disk Usage Analyzer With Ncurses Interface
     nerd-fonts.fira-code # Nerd Font fira-code
     nerd-fonts.jetbrains-mono # Nerd Font jetbrains-mono
+    pkg-config # Wrapper Script For Allowing Packages To Get Info On Others
     rhythmbox # Goto Music Listening App until I make one
+    ripgrep # Improved Grep
     scrcpy # Display & Control Android Device
     qbittorrent # Aye Aye Captain
+    v4l-utils # Used For Things Like OBS Virtual Camera
     wget # Tool For Fetching Files With Links
     wl-clipboard # Clipboard helper for wayland
     vscode # Noo VsCode
