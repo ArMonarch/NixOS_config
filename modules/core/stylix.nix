309308239484c8c29f828b5f30e512d2544a8735
ArMonarch <praffulthapa12@gmail.com>
2025-07-26 21:06:22 +0545

    feat: a certain mix of image generated theme and snazzy theme

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -29,19 +29,39 @@ in {
     # };
 
     # Snazzy base16Scheme
+    # base16Scheme = {
+    #   "base00" = "282a36";
+    #   "base01" = "34353e";
+    #   "base02" = "43454f";
+    #   "base03" = "78787e";
+    #   "base04" = "a5a5a9";
+    #   "base05" = "e2e4e5";
+    #   "base06" = "eff0eb";
+    #   "base07" = "f1f1f0";
+    #   "base08" = "ff5c57";
+    #   "base09" = "ff9f43";
+    #   "base0A" = "f3f99d";
+    #   "base0B" = "5af78e";
+    #   "base0C" = "9aedfe";
+    #   "base0D" = "57c7ff";
+    #   "base0E" = "ff6ac1";
+    #   "base0F" = "b2643c";
+    # };
+
+    # My color scheme
     base16Scheme = {
-      "base00" = "282a36";
-      "base01" = "34353e";
-      "base02" = "43454f";
-      "base03" = "78787e";
-      "base04" = "a5a5a9";
-      "base05" = "e2e4e5";
-      "base06" = "eff0eb";
-      "base07" = "f1f1f0";
-      "base08" = "ff5c57";
+      "base00" = "1a1b26";
+      "base01" = "3f484d";
+      "base02" = "656b69";
+      "base03" = "c78f8e";
+      "base04" = "c1b8a9";
+      "base05" = "cee7ee";
+      "base06" = "ecf4e5";
+      "base07" = "ffe9f9";
+      "base08" = "968ca5";
       "base09" = "ff9f43";
       "base0A" = "f3f99d";
-      "base0B" = "5af78e";
+      "base0B" = "fa5f48";
       "base0C" = "9aedfe";
       "base0D" = "57c7ff";
       "base0E" = "ff6ac1";
@@ -66,12 +86,5 @@ in {
         name = "DejaVu Serif";
       };
     };
-
-    # fonts.sizes = {
-    #   applications = 10;
-    #   terminal = 15;
-    #   desktop = 10;
-    #   popups = 10;
-    # };
   };
 }
