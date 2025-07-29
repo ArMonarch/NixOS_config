3ed0a74aa4fd763f0165389d90b99c36f696064b
ArMonarch <praffulthapa12@gmail.com>
2025-07-29 21:15:40 +0545

    feat: font added to stylix

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -9,28 +9,30 @@ in {
     targets = {
       console.enable = true;
       grub.enable = true;
+      gnome.enable = true;
     };
 
-    # TODO: Combine these two base16-schemes
     base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
 
     image = stylixImage;
 
-    polarity = "either";
-
     fonts = {
       monospace = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Sans Mono";
+        package = pkgs.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font Mono";
       };
+
       sansSerif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Sans";
+        package = pkgs.adwaita-fonts;
+        name = "Adwaita Sans";
       };
+
       serif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Serif";
+        package = pkgs.adwaita-fonts;
+        name = "Adwaita Sans";
       };
     };
+
+    polarity = "dark";
   };
 }
