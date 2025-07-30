1f08b0c52697227af8603e575bd590751ccf0883
ArMonarch <praffulthapa12@gmail.com>
2025-07-30 08:44:44 +0545

    feat: gtk coonfig tweaks

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -16,5 +16,18 @@
       name = lib.mkForce "rose-pine-gtk";
       package = lib.mkForce pkgs.rose-pine-gtk-theme;
     };
+
+    # TODO: Icon Theme
+    # iconTheme = {
+    #   name = "Papirus-Dark";
+    #   package = pkgs.papirus-icon-theme;
+    # };
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
   };
 }
