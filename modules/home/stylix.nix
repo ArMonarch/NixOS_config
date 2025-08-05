97fe3b1566305395081256a3c6877eac0c32c71d
ArMonarch <praffulthapa12@gmail.com>
2025-08-05 22:30:11 +0545

    feat: reduced gtk apps titlebar height just right.

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -23,54 +23,10 @@ _: {
           }
 
           windowhandle box {
-            padding-top:0px;
+            padding-top:1px;
             padding-bottom:0px;
           }
         '';
-
-        # extraCss = ''
-        #   headerbar {
-        #       min-height: 10px;
-        #       padding: 0px;
-        #   }
-        #
-        #   headerbar entry,
-        #   headerbar spinbutton,
-        #   headerbar button,
-        #   headerbar separator {
-        #       margin-top: 0px;
-        #       margin-bottom: 0px;
-        #       padding: 2px;
-        #       min-width: 0px;
-        #       min-height: 0px;
-        #   }
-        #   headerbar windowhandle {
-        #       margin-top: 0px;
-        #       margin-bottom: 0px;
-        #       min-height: 8px;
-        #       padding: 0px;
-        #   }
-        #   headerbar windowhandle box {
-        #       margin-top: 1px;
-        #       margin-bottom: 1px;
-        #       margin-right: 4px;
-        #       margin-left: 4px;
-        #       padding: 0px;
-        #   }
-        #   headerbar windowhandle label {
-        #       padding: 1px;
-        #   }
-        #
-        #   headerbar windowhandle box.end button image  {
-        #       min-height: 8px;
-        #   }
-        #
-        #   .default-decoration {
-        #       min-height: 0; /* let the entry and button drive the titlebar size */
-        #       padding: 1px;
-        #       margin-bottom: 0px;
-        #   }
-        # '';
       };
     };
   };
