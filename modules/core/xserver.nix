15ab2ef9107d5e845e61d32826faa2f9c4bb063f
ArMonarch <praffulthapa12@gmail.com>
2025-11-14 19:37:02 +0545

    chore: removed console.keymap

--- modules/core/xserver.nix
+++ modules/core/xserver.nix
@@ -2,11 +2,9 @@
   inherit (import ../../profiles/legion/variables.nix) keyboardLayout consoleKeyMap;
 in {
   services.xserver = {
-    # Disable the X11 windowing system, and use Wayland Explicityly
     enable = true;
 
     xkb.layout = "${keyboardLayout}";
     xkb.variant = "";
   };
-  console.keyMap = "${consoleKeyMap}"; # NOTE: Needed ?
 }
