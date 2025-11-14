2afb15ab715244fccf1924029380d82e97b56aa4
ArMonarch <praffulthapa12@gmail.com>
2025-11-14 19:36:41 +0545

    feat: needed only qemu for virtulization

--- modules/core/virtualisation.nix
+++ modules/core/virtualisation.nix
@@ -4,24 +4,6 @@
   pkgs,
   ...
 }: {
-  # Enable dconf (System Management Tool)
-  programs.dconf.enable = true;
-
-  # Add user to libvirtd group
-  users.users.${host}.extraGroups = ["libvirtd"];
-
-  # Install necessary packages
-  environment.systemPackages = with pkgs; [
-    virt-manager
-    virt-viewer
-    spice
-    spice-gtk
-    spice-protocol
-    win-virtio
-    win-spice
-    adwaita-icon-theme
-  ];
-
   # Manage the virtualisation services
   virtualisation = {
     libvirtd = {
@@ -33,5 +15,6 @@
     };
   };
 
-  services.spice-vdagentd.enable = true;
+  # Add user to libvirtd group
+  users.users.${host}.extraGroups = ["libvirtd"];
 }
