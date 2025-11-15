63298a41019f6cd65827bb798120aea978d6a009
ArMonarch <praffulthapa12@gmail.com>
2025-11-15 13:24:56 +0545

    feat: removed gtk3/4 css from stylix

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -6,27 +6,6 @@ _: {
     targets = {
       gtk = {
         enable = true;
-        extraCss = ''
-          window.ssd headerbar.titlebar {
-            padding-top: 2px;
-            padding-bottom: 2px;
-            min-height: 0;
-          }
-          window.ssd headerbar.titlebar button.titlebutton {
-              padding: 1px;
-              min-height: 0;
-              min-width: 0;
-          }
-
-          headerbar {
-            min-height:20px;
-          }
-
-          windowhandle box {
-            padding-top:1px;
-            padding-bottom:0px;
-          }
-        '';
       };
     };
   };
