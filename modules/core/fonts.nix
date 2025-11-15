0337a8ff35ba1f0ec5e2a5272cf0fc18e1717c53
ArMonarch <praffulthapa12@gmail.com>
2025-11-15 13:32:12 +0545

    feat: removed unecessayr fonts.

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -3,13 +3,8 @@
     packages = with pkgs; [
       fira-code
       fira-code-symbols
-      font-awesome
       jetbrains-mono
       noto-fonts
-      noto-fonts-emoji
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      noto-fonts-monochrome-emoji
       roboto
       roboto-mono
     ];
