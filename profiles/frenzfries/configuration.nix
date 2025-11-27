a46bc12a3c52c8f10102d7f6c2feed44469327d4
ArMonarch <praffulthapa12@gmail.com>
2025-11-27 23:02:41 +0545

    feat: changed default frenzfries desktop manager and display manager to sddm and plasma.

--- profiles/frenzfries/configuration.nix
+++ profiles/frenzfries/configuration.nix
@@ -44,8 +44,10 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = false;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.gnome.enable = false;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
