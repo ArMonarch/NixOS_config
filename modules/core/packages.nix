825d84459bb639bd371035b8dc36eed056f16ecd
ArMonarch <praffulthapa12@gmail.com>
2025-11-28 17:56:01 +0545

    feat: changed some packages to systemPackages from defaultPackages.

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -1,8 +1,6 @@
 {pkgs, ...}: {
   programs = {
     firefox.enable = true;
-    # PERF: Gibrish print for command-not-found in fish,
-    # fixed by disabling command-not-found.
     command-not-found.enable = false;
   };
 
@@ -35,22 +33,25 @@
     allowBroken = true;
   };
 
+  environment.systemPackages = with pkgs; [
+    htop # Simple Terminal Based System Monitor
+    git # version manager, Configured through home-manager
+    killall # For Killing All Instances Of Programs
+    pkg-config # Wrapper Script For Allowing Packages To Get Info On Others
+    ripgrep # Improved Grep
+    tldr # what man page wish it could be
+    wget # Tool For Fetching Files With Links
+  ];
+
   environment.defaultPackages = with pkgs; [
     fd # best alternative for find
     fzf # Command line fuzzy finder
-    git # version manager, Configured through home-manager
-    htop # Simple Terminal Based System Monitor
-    killall # For Killing All Instances Of Programs
     libnotify # For Notifications
     nerd-fonts.fira-code # Nerd Font fira-code
     nerd-fonts.jetbrains-mono # Nerd Font jetbrains-mono
-    pkg-config # Wrapper Script For Allowing Packages To Get Info On Others
-    ripgrep # Improved Grep
     scrcpy # Display & Control Android Device
-    tldr # what man page wish it could be
     postgresql_16 # Postgres database
     v4l-utils # Used For Things Like OBS Virtual Camera
-    wget # Tool For Fetching Files With Links
     wl-clipboard # Clipboard helper for wayland
   ];
 }
