9d09cc854bf13d68e56438a270c8a8b29bcc1384
ArMonarch <praffulthapa12@gmail.com>
2025-11-29 19:30:13 +0545

    feat: changed stylix theme to rose-pine-moon

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -10,7 +10,7 @@
       gnome.enable = true;
     };
 
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
 
     fonts = {
       monospace = {
